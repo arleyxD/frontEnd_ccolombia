b70ba046114a65019afe26d1e226f78027d405f7
CORTAVO2015 <gustavoancortes@gmail.com>
2023-08-03 21:32:44 -0500

    [+]Se agrego alerta de producto agotado se dio estilos y se agragaron mas tarjetas con productos

--- src/app/publico/pages/fil-product/fil-product-boton/fil-product-boton.component.ts
+++ src/app/publico/pages/fil-product/fil-product-boton/fil-product-boton.component.ts
@@ -1,4 +1,7 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { TiendaService } from 'src/app/auth/services/tienda.service';
+
 
 @Component({
   selector: 'app-fil-product-boton',
@@ -6,6 +9,32 @@ import { Component, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./fil-product-boton.component.css']
 })
 export class FilProductBotonComponent {
+  myControl = new FormControl();
+  options: string[] = [];
+  filteredOptions: string[] = [];
+  selectedOption: string = '';
+  
+  constructor(private tiendaService: TiendaService) { }
+
+  ngOnInit() {
+    
+    this.tiendaService.getStores().subscribe(stores => {
+      this.options = stores;
+    });
+
+    this.myControl.valueChanges.subscribe(value => {
+      this.filteredOptions = this._filter(value);
+    });
+  }
+
+  onOptionClick(option: string) {
+    this.selectedOption = option;
+    this.myControl.setValue(option);
+  }
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
   
   @Output() filtroSeleccionado = new EventEmitter<string>();
 
