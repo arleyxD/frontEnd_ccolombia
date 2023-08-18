6bc568dcfef8c2f8e5c3d2eb21e1a977c7456349
arleyxD <castearley@gmail.com>
2023-08-18 07:40:35 -0500

    Se arregla las imagnes en tiendas

--- src/app/publico/shared/tarjetas/tarjetas.component.ts
+++ src/app/publico/shared/tarjetas/tarjetas.component.ts
@@ -9,14 +9,34 @@ import { TiendaService } from 'src/app/auth/services/tienda.service';
 })
 export class TarjetasComponent implements OnInit{
 
+  private images=[
+    {src:'../../../../assets/imgTiendas/tiendaE.jpg'},
+    {src:'../../../../assets/imgTiendas/tiendaF.jpg'},
+    {src:'../../../../assets/imgTiendas/tiendaO.jpg'},
+    {src:'../../../../assets/imgTiendas/tiendaP.jpg'},
+    {src:'../../../../assets/imgTiendas/tiendaR.jpg'},
+    {src:'../../../../assets/imgTiendas/tiendaX.jpg'}
+  ];
+
+
   tiendas: Tienda[]=[];
-  constructor(private tiendaService: TiendaService){}
+  constructor(private tiendaService: TiendaService){
+  }
 
   ngOnInit(): void {
     this.tiendaService.getTiendas().subscribe(data =>{
       this.tiendas= data.produt || [];
       console.log(this.tiendas);
     });
+    this.updateRandomImage();
   }
+
+  updateRandomImage() {
+    const r= Math.floor(Math.random() * (this.images.length - 1)) + 0;
+    console.log(this.images[r].src)
+    return this.images[r].src;
+  }
+
+  
 
 }
